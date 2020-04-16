768b246fcddd08b0d2b0332a054b61a49590c0c2
Kothagattu <181589@corpaa.aa.com>
2020-04-16 14:44:26 -0500

    test results by Praneeth

---
+++ sql-q1.sql
@@ -0,0 +1,7 @@
+SELECT name as carrier , MAX(PRICE) AS max_price
+FROM Flights JOIN Carriers
+ON Flights.carrier_id = Carriers.cid
+WHERE (Flights.origin_city LIKE 'Seattle%' OR Flights.origin_city LIKE 'New York%')
+AND ( Flights.dest_city LIKE 'New York%' OR Flights.dest_city LIKE 'Seattle%')
+GROUP BY Carriers.name
+ORDER BY 2 DESC;
---
+++ sql-q3.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT dest_city from Flights 
+WHERE  dest_city NOT IN
+      (SELECT DISTINCT dest_city from Flights 
+       WHERE origin_city IN
+        (SELECT DISTINCT dest_city from Flights 
+         WHERE origin_city LIKE 'Seattle%')
+       )
+ORDER BY 1;
