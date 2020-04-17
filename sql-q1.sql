beb32c81fb93ea5eab544b9025f6be6a242606b5
kothagattupraneeth <praneethece@gmail.com>
2020-04-17 00:54:03 -0500

    Update sql-q1.sql

--- sql-q1.sql
+++ sql-q1.sql
@@ -4,4 +4,4 @@ ON Flights.carrier_id = Carriers.cid
 WHERE (Flights.origin_city LIKE 'Seattle%' OR Flights.origin_city LIKE 'New York%')
 AND ( Flights.dest_city LIKE 'New York%' OR Flights.dest_city LIKE 'Seattle%')
 GROUP BY Carriers.name
-ORDER BY 2 DESC;
+ORDER BY 1,2;
