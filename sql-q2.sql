16472525ccb9b244d98c4170847a7c322a4554f9
kothagattupraneeth <praneethece@gmail.com>
2020-04-17 00:54:43 -0500

    Update sql-q2.sql

--- sql-q2.sql
+++ sql-q2.sql
@@ -1,9 +1,8 @@
-SELECT flight_num
+SELECT DISTINCT flight_num
 FROM Flights JOIN Carriers
 ON Flights.carrier_id = Carriers.cid
 JOIN Weekdays
 ON Flights.day_of_week_id = Weekdays.did
 WHERE Flights.origin_city LIKE 'Seattle%' AND Flights.dest_city LIKE 'BOSTON%'
 AND Carriers.name LIKE '%Alaska%'
-AND Weekdays.day_of_week = 'Monday'
-GROUP BY flight_num;
+AND Weekdays.day_of_week = 'Monday';
